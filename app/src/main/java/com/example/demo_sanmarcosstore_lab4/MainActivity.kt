99d6ef15fa4a2c0b33fe4ee5f064bdbb313a8a61
Mauricio Jimmy Juarez Zeballos <mauricio.juarez@unmsm.edu.pe>
2026-07-09 15:46:26 -0500

    first commit

--- app/src/main/java/com/example/demo_sanmarcosstore_lab4/MainActivity.kt
+++ app/src/main/java/com/example/demo_sanmarcosstore_lab4/MainActivity.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.demo_sanmarcosstore_lab3.ui.theme.AppTheme
-
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.demo_sanmarcosstore_lab3.data.database.AppDatabaseProvider
+import com.example.demo_sanmarcosstore_lab3.data.repository.ProductoRepository
+import com.example.demo_sanmarcosstore_lab3.viewmodel.ProductoViewModel
+import com.example.demo_sanmarcosstore_lab3.viewmodel.ProductoViewModelFactory
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
